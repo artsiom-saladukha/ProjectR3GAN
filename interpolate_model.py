c27c54911772fac246fd242c80f8bb6accdbe328
artsiom-saladukha <artsiom-saladukha@yandex.ru>
2025-04-29 11:25:51 +0300

    fixed model interpolation

--- interpolate_model.py
+++ interpolate_model.py
@@ -11,16 +11,36 @@ import torch
 import legacy
 
 
-def interpolate_model(G,
-                      label,
-                      z1,
-                      z2,
+def interpolate_latent_space(G,
+                             label,
+                             z1,
+                             z2,
+                             num_steps=10):
+    alphas = torch.linspace(0, 1, num_steps)
+    interpolated_images = []
+
+    for alpha in alphas:
+        z_interpolated = alpha * z1 + (1 - alpha) * z2
+        z_interpolated = z_interpolated.to('cuda')
+        with torch.no_grad():
+            image = G(z_interpolated, label)
+            interpolated_images.append(image)
+    return interpolated_images
+
+
+@click.command()
+@click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
+@click.option('--outdir', help='Where to save the output images', type=str, required=True, metavar='DIR')
+def interpolate_model(network_pkl,
+                      outdir,
                       num_steps=10):
+    device = 'cuda'
     with dnnlib.util.open_url(network_pkl) as f:
         G = legacy.load_network_pkl(f)['G_ema'].to(device)
     os.makedirs(outdir, exist_ok=True)
     
     label = torch.zeros([1, G.c_dim], device=device)
+    class_idx = 0
     if G.c_dim != 0:
         if class_idx is None:
             raise click.ClickException('Must specify class label with --class when using a conditional network')
@@ -29,7 +49,15 @@ def interpolate_model(G,
         if class_idx is not None:
             print ('warn: --class=lbl ignored when running on an unconditional network')
     
-    z1 = torch.from_numpy(np.random.RandomState(1).randn(1, G.z_dim)).to(device)
-    z2 = torch.from_numpy(np.random.RandomState(2).randn(1, G.z_dim)).to(device)
+    z1 = torch.from_numpy(np.random.RandomState(0).randn(1, G.z_dim)).to(device)
+    z2 = torch.from_numpy(np.random.RandomState(42).randn(1, G.z_dim)).to(device)
+    
     interpolated_images = interpolate_latent_space(G, label, z1, z2, num_steps=10)
+    for idx, image in enumerate(interpolated_images):
+        image = (image.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+        PIL.Image.fromarray(image[0].cpu().numpy(), 'RGB').save(f'{outdir}/{idx}.png')
     return interpolated_images
+
+
+if __name__ == "__main__":
+    interpolate_model()
